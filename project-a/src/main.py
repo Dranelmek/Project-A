1190a4cb407d0ac8530a31d63a547c09072aec72
Lenard <bebobhd@gmail.com>
2025-04-23 00:46:11 +0100

    added constants to settings

--- project-a/src/main.py
+++ project-a/src/main.py
@@ -5,7 +5,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("My Game")
+        pygame.display.set_caption("Project Angel")
         self.clock = pygame.time.Clock()
         self.running = True
 
---
+++ project-a/src/settings.py
@@ -0,0 +1,11 @@
+# game setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+TILESIZE = 64
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+WORLD_MAP = []
